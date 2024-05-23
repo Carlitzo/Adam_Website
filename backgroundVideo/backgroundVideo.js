429294bdb81618201d22b81dd8e0bd2c629332b6
Carlitzo <adrianscholl95@hotmail.com>
2024-05-23 14:07:28 +0200

    Created filestructure, intial examples of background, as well as some css

---
+++ backgroundVideo/backgroundVideo.js
@@ -0,0 +1,7 @@
+function renderBackgroundVideo(parent) {
+    parent.innerHTML = `
+        <video autoplay muted loop id="backgroundVideo">
+            <source src="media/Videos/Video For Adrian.mp4" type="video/mp4">
+        </video>
+    `
+}
